18f07e2aa1440f0fc2497ae96b3c17723a669073
mvladt <v-mell@yandex.ru>
2025-11-16 13:43:13 +0300

    Добавил в Vitest 'browser mode'
    Провайдер — Playwright. Также добавил 'hello world' компонент для теста.

--- vitest.config.js
+++ vitest.config.js
@@ -1,3 +1,18 @@
 import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+import { playwright } from "@vitest/browser-playwright";
 
-export default defineConfig({});
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    browser: {
+      enabled: true,
+      headless: true,
+      provider: playwright(),
+      instances: [{ browser: "chromium" }],
+      include: ["tests/browser/**/*"],
+    },
+
+    exclude: ["tests/browser/**"],
+  },
+});
