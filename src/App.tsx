942c1e4e1195016ce9ec9662cb7b0e88ee22a691
mvladt <v-mell@yandex.ru>
2025-11-20 20:00:47 +0300

    Переделал 'TaskHeader'
    Вынес зависимость стора. Для удобства тестирования.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,37 @@
 import { useState } from "react";
 
 import TaskHeader from "./components/TaskHeader.js";
-import { TasksProvider } from "./stores/tasks/TasksContext.js";
+import { TasksProvider, useTasksStore } from "./stores/tasks/TasksContext.js";
 import TaskList from "./components/TaskList.js";
 import { TaskModalProvider } from "./stores/taskModal/TaskModalContext.js";
 import TaskModal from "./components/TaskModal.js";
 
 function App() {
+  const { dispatch, actions, numberOfCompleted } = useTasksStore();
+
   const [isShow, setShow] = useState(false);
+
+  const onToggleHeader = () => {
+    setShow(!isShow);
+  };
+
+  const onClearHeader = () => {
+    if (confirm("Удалить все завершенные?")) {
+      dispatch(actions.deleteCompletedOnes());
+    }
+  };
 
   return (
     <>
       <TasksProvider>
         <TaskModalProvider>
-          <TaskHeader isShow={isShow} showToggle={() => setShow(!isShow)} />
+          <TaskHeader
+            title="Все"
+            numberOfCompleted={numberOfCompleted}
+            isShow={isShow}
+            onToggle={onToggleHeader}
+            onClear={onClearHeader}
+          />
           <TaskList isShow={isShow} />
           <TaskModal />
         </TaskModalProvider>
--- src/components/TaskHeader.tsx
+++ src/components/TaskHeader.tsx
@@ -1,36 +1,32 @@
-import { useTasksStore } from "../stores/tasks/TasksContext.js";
-
 type Props = {
+  title: string;
   isShow: boolean;
-  showToggle(): void;
+  numberOfCompleted: number;
+  onToggle(): void;
+  onClear(): void;
 };
 
-export default function TaskHeader({ isShow, showToggle }: Props) {
-  const title = "Все";
-  const { dispatch, actions, numberOfCompleted } = useTasksStore();
-
-  const onClear = (): void => {
-    if (confirm("Удалить все завершенные?")) {
-      dispatch(actions.deleteCompletedOnes());
-    }
-  };
-
-  const onShowToggle = (): void => {
-    showToggle();
-  };
-
+export default function TaskHeader({
+  title,
+  isShow,
+  numberOfCompleted,
+  onToggle,
+  onClear,
+}: Props) {
   return (
     <>
-      <h1>{title}</h1>
-      <div>
-        <span>{numberOfCompleted} завершены</span>•
-        <button type="button" onClick={onClear}>
-          Очистить
-        </button>
-        <button type="button" onClick={onShowToggle}>
-          {isShow ? "Скрыть" : "Показать"}
-        </button>
-      </div>
+      <header style={{ marginBottom: "1rem" }}>
+        <h1>{title}</h1>
+        <div>
+          <span>{numberOfCompleted} завершены</span>•
+          <button type="button" onClick={onClear}>
+            Очистить
+          </button>
+          <button type="button" onClick={onToggle}>
+            {isShow ? "Скрыть" : "Показать"}
+          </button>
+        </div>
+      </header>
     </>
   );
 }
